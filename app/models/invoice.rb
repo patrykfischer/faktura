7aaa8e359e20b1ee980586203e551a2aaf667466
patrykfischer <fischer.patryk@gmail.com>
2014-05-15 21:00:59 +0200

    Add validation to invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ActiveRecord::Base
-
+validates :number_of_invoice, :date_of_sold, :data_of_sold, :data_build, :method_of_paymant,
+:id_own_company, :id_invoice_company, presence: true
 end
