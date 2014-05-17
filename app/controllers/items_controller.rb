22a42a7f76a852813e8e750f1f04bca96b8bb88e
patrykfischer <fischer.patryk@gmail.com>
2014-05-17 02:27:24 +0200

    standart controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,17 @@
 class ItemsController < ApplicationController
+  expose(:items, attributes: :items_params)
 
+  def create
+    if item.save
+      render action: 'index'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name_of_service, :qty, :unit, :unit_net_price)
+  end
 end
