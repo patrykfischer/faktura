d4e384ba225ee08ea234bbf4c6da1912964babe6
patrykfischer <fischer.patryk@gmail.com>
2014-05-17 19:20:43 +0200

    item require session

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   expose(:item, attributes: :items_params)
+  before_action :require_login
 
   def create
     if item.save
@@ -13,5 +14,13 @@ class ItemsController < ApplicationController
 
   def items_params
     params.require(:item).permit(:name_of_service, :qty, :unit, :unit_net_price)
+  end
+
+  def require_login
+    if !session[:user_id]
+      redirect_to new_session_path, notice: "Musisz być zalogowany !!"
+    else
+      @user_id = session[:user_id]
+    end
   end
 end
