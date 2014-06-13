263ea6b7b09c4d9ccb9aead9e200e9b1be9f86e5
patrykfischer <fischer.patryk@gmail.com>
2014-06-13 22:31:34 +0200

    add user_id validate

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ActiveRecord::Base
-belongs_to :user
-has_many :items
-validates :number_of_invoice, :data_of_sold, :data_build, :method_of_payment,
-:id_own_company, :id_invoice_company, presence: true
-validates :number_of_invoice, :uniqueness => {:message => "Faktura z tym numerem jest już wystawiona!"}
+  belongs_to :user
+  has_many :items
+  validates :number_of_invoice, :data_of_sold, :data_build, :method_of_payment,
+    :id_own_company, :id_invoice_company, :user_id, presence: true
+  validates :number_of_invoice, :uniqueness => {:message => "Faktura z tym numerem jest już wystawiona!"}
 end
