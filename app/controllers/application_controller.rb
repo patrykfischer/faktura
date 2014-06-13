a53870736a4d0cd781a0863038a62ab6395aee7b
patrykfischer <fischer.patryk@gmail.com>
2014-06-13 21:25:43 +0200

    add require login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
+
+  private
+
+  def require_login
+    if !session[:user_id]
+      redirect_to new_session_path, notice: "Musisz być zalogowany !!"
+    else
+      @user = User.find_by(id: session[:user_id])
+    end
+  end
+
 end
