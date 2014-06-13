02e17486ca74c770f51587ae97305b6773c01365
patrykfischer <fischer.patryk@gmail.com>
2014-06-13 22:18:29 +0200

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 	expose(:user, attributes: :user_params)
 
+	before_filter :require_login, only: [:index]
+
 	def create
 		if user.save
 			session[:user_id] = user.id
@@ -10,21 +12,9 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def index
-		require_login
-	end
-
 	private
 
 	def user_params
 		params.require(:user).permit(:username, :password, :password_confirmation)
-	end
-
-	def require_login
-		if !session[:user_id]
-			redirect_to new_session_path, notice: "Musisz być zalogowany !!"
-		else
-			@user_id = session[:user_id]
-		end
 	end
 end
