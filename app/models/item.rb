a21d28635cb08a55dc49860086fabc49d65604de
patrykfischer <fischer.patryk@gmail.com>
2014-06-14 00:38:54 +0200

    add validate to vat_rate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :invoice
-  validates :name_of_service, :qty, :unit, :unit_net_price, :invoice_id, presence: true
+  validates :name_of_service, :qty, :unit, :unit_net_price, :invoice_id, :vat_rate, presence: true
 end
