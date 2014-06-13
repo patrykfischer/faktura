0348f86daaf52a49c1640c28f820b6c11b0a4ec3
patrykfischer <fischer.patryk@gmail.com>
2014-06-13 21:42:13 +0200

    add new routes to item new action with invoice_id

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Faktura::Application.routes.draw do
   resources :invoices
   resources :items
 
-  get "logout" => "sessions#destroy"
+  get 'invoices/:invoice_id/items/new'  => 'items#new', as: 'new_items'
+
+  get 'logout' => 'sessions#destroy'
   root to: 'users#index'
 end
