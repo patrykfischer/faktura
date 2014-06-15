a1a6b31f2c1e304033504db582b3448c4b7d2f00
patrykfischer <fischer.patryk@gmail.com>
2014-06-16 01:04:48 +0200

    rebuild controller after change routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
   expose(:item, attributes: :item_params)
   before_action :require_login
-  before_action :current_invoice
 
   def create
-    item.invoice_id = @@current_invoice.id
+    item.invoice_id = params[:invoice_id]
     if item.save
       count_value
       redirect_to  invoice_items_path if item.save
@@ -14,21 +13,13 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.where(invoice_id: @@current_invoice.id)
+    @items = Item.where(invoice_id: params[:invoice_id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name_of_service, :qty, :unit, :unit_net_price, :vat_rate, :pkwiu)
-  end
-
-  def current_invoice
-    if params[:invoice_id]
-      @@current_invoice = Invoice.find_by(id: params[:invoice_id])
-    else
-      @@current_invoice
-    end
   end
 
   def count_value
