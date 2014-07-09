2d602383afcca2570dd6904783d84d5b3f380733
patrykfischer <fischer.patryk@gmail.com>
2014-07-09 16:11:18 +0200

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(username: 'p', password: 'p', password_confirmation: 'p')
+company = Company.create(user_id: User.first.id, company_name: 'Black', nip: '111-111-11-11',
+  place_of_taxes: 'UK', city: 'London', street:'London-Street', zip_code:'12-122', phone_number: '123-123-123')
+company = Company.create(user_id: User.first.id, company_name: 'White', nip: '111-111-11-11',
+  place_of_taxes: 'UK', city: 'London', street:'London-Street', zip_code:'12-122', phone_number: '123-123-123')
+invoice = Invoice.create(user_id: User.first.id, number_of_invoice: Invoice.any? ? Invoice.last.number_of_invoice + 1 : 1,
+  data_of_sold: '2014-01-01', data_build: '2014-02-02', method_of_payment: 'Gotówka', own_company_id: Company.first.id,
+  other_company_id: Company.last.id)
+item = Item.create(invoice_id: Invoice.first.id, name_of_service: 'Malowanie Fasady', qty: 10,
+  unit: 'm2', unit_net_price: 12.99, vat_rate: 0.22)
