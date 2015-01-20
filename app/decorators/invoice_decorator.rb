6552eec848c1bc1a04dd758a6fdd5947d74a4d63
patrykfischer <fischer.patryk@gmail.com>
2015-01-21 00:49:08 +0100

    fix problem with summation table

--- app/decorators/invoice_decorator.rb
+++ app/decorators/invoice_decorator.rb
@@ -3,37 +3,37 @@ class InvoiceDecorator < Draper::Decorator
 
   def net_price_8
     count = 0
-    get_items_vat_rate_8.each { |item| count =+ item.total_net_price }
+    get_items_vat_rate_8.each { |item| count += item.total_net_price }
     count
   end
 
   def vat_rate_8
     count = 0
-    get_items_vat_rate_8.each { |item| count =+ item.vat_amount }
+    get_items_vat_rate_8.each { |item| count += item.vat_amount }
     count
   end
 
   def net_price_23
     count = 0
-    get_items_vat_rate_8.each { |item| count =+ item.total_net_price }
+    get_items_vat_rate_23.each { |item| count += item.total_net_price }
     count
   end
 
   def vat_rate_23
     count = 0
-    get_items_vat_rate_23.each { |item| count =+ item.vat_amount }
+    get_items_vat_rate_23.each { |item| count += item.vat_amount }
     count
   end
 
   def price_total_8
     count = 0
-    get_items_vat_rate_8.each { |item| count =+ item.total_gross_price }
+    get_items_vat_rate_8.each { |item| count += item.total_gross_price }
     count
   end
 
   def price_total_23
     count = 0
-    get_items_vat_rate_8.each { |item| count =+ item.total_gross_price }
+    get_items_vat_rate_23.each { |item| count += item.total_gross_price }
     count
   end
 
@@ -46,13 +46,13 @@ class InvoiceDecorator < Draper::Decorator
   end
 
   def summation_total
-    price_total_23 + price_total_8
+    summation_vat + summation_net
   end
 
   private
 
   def get_items_vat_rate_8
-    invoice.items.where(vat_rate: '0.8')
+    invoice.items.where(vat_rate: '0.08')
   end
 
   def get_items_vat_rate_23
